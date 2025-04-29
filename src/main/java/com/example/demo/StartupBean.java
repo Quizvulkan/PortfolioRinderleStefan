1bfc7fa831213da52c6c96f6460976205a467eb7
dorian <dorian.produktiv@gmail.com>
2025-04-29 16:31:18 +0200

    test

---
+++ src/main/java/com/example/demo/StartupBean.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import jakarta.annotation.PostConstruct;
+import org.openapitools.client.api.DefaultApi;
+import org.openapitools.client.model.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class StartupBean {
+
+    @PostConstruct
+    public void init(){
+        DefaultApi defaultApi = new DefaultApi();
+
+        GameInputDto gameInput = new GameInputDto();
+        gameInput.setGroupName("IrgendeinName"); //Group name
+
+        GameDto response = defaultApi.gamePost(gameInput);
+
+        System.out.println(response);
+
+    }
+}
